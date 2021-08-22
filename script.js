522f0d380b2dcd9c8a8255bc6844cdcfe7a1d321
Guilherme C. Tonello <guitonello@outlook.com.br>
2021-08-22 20:11:44 -0300

    Gerando imagens aleatórias e scroll infinito

---
+++ script.js
@@ -0,0 +1,25 @@
+function gerarImagem() {
+    let fotos = document.querySelector('div.fotos');
+    fetch('https://picsum.photos/500/300')
+    .then(resp => {
+        let imagem = resp.url;
+        fotos.insertAdjacentHTML('beforeend', `
+            <div class="foto">
+                <img src=${imagem} loading="lazy" />
+            </div>
+        `);
+    });
+}
+
+window.addEventListener('load', () => {
+    for (let i = 0; i < 3; i++) {
+        gerarImagem();
+    }
+});
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY + window.innerHeight >= document.body.offsetHeight) {
+        console.log('aqui');
+        gerarImagem();
+    }
+});
